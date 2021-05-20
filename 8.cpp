7b2da0f28abcb25cbde6366204b2cbf9dfda5271
Dulguun123456 <84019532+Dulguun123456@users.noreply.github.com>
2021-05-20 22:30:49 +0800

    Add files via upload

---
+++ 8.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char name;
+	
+	cout<<"Egshig giigvvlegchiign shalgay: ";
+	cin>>name;
+	if(name=='a'|| name=='A'|| name=='e' || name=='E' || name=='i'|| name=='I'|| name=='o'|| name=='O'|| name=='u' || name=='U'){
+	cout<<name<<" it is a vowel";	
+	}else{
+		cout<<name<<" it is a not vowel";
+	}
+	
+	return 0;
+}
